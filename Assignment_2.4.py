3ba8cebae10e2b255cacc36c11aef86bbec25607
bauassr <bauassr@gmail.com>
2018-03-14 15:45:37 +0530

    Add files via upload

---
+++ Assignment_2.4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 10:49:15 2018
+
+@author: singh.shivam
+"""
+
+print("WE,THE PEOPLE OF INDIA,\n   having solemnly resolved to constitute India into a")
+print("\t  SOVEREIGN ,SOCIALIST,SECULAR,DEMOCRATIC REPUBLIC")
+print("\t   and to secure to all its citizens:")
