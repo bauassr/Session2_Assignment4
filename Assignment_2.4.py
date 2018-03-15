edeba0b20738c2bc4c9e93aff1f263f013d7afc8
bauassr <bauassr@gmail.com>
2018-03-15 10:41:10 +0530

    Update Assignment_2.4.py

--- Assignment_2.4.py
+++ Assignment_2.4.py
@@ -5,6 +5,6 @@ Created on Wed Mar 14 10:49:15 2018
 @author: singh.shivam
 """
 
-print("WE,THE PEOPLE OF INDIA,\n     having solemnly resolved to constitute India into a")
-print("\t  SOVEREIGN ,SOCIALIST,SECULAR,DEMOCRATIC REPUBLIC")
+print("WE,THE PEOPLE OF INDIA,\n     having solemnly resolved to constitute India into a SOVEREIGN,!")
+print("\t  SOCIALIST,SECULAR,DEMOCRATIC REPUBLIC")
 print("\t   and to secure to all its citizens:")
